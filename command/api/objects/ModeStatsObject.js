f8dd5436f65efb837786518284301ff9baf1713f
Starlight Exsper <exsper@163.com>
2020-03-19 23:24:37 +0800

    modify description

--- command/api/objects/ModeStatsObject.js
+++ command/api/objects/ModeStatsObject.js
@@ -33,8 +33,8 @@ class ModeStatsObject {
         output = output + "acc：" + this.accuracy + "%\n";
         output = output + "等级：" + this.level + "\n";
         output = output + "pp：" + this.pp + "\n";
-        output = output + "全球排名：" + this.rank + "\n";
-        output = output + "本地排名：" + this.countryRank + "\n";
+        output = output + "全服排名：#" + this.rank + "\n";
+        //output = output + "本地排名：" + this.countryRank + "\n";
         output = output + "游玩次数：" + this.playcount + "\n";
         output = output + "rank总分：" + utils.format_number(this.rankedScores) + "\n";
         output = output + "游戏时长：" + utils.getUserTimePlayed(this.play_time) + "\n";
@@ -46,8 +46,8 @@ class ModeStatsObject {
     addCompareString(nowValue, oldValue, digits = 0) {
         const multiplier = Math.pow(10, digits);
         let delta = (nowValue * multiplier - oldValue * multiplier) / multiplier;
-        if (delta > 0) return " \t ( +" + delta + " )\n";
-        else if (delta < 0) return " \t ( " + delta + " )\n";
+        if (delta > 0) return " \t ( +" + delta.toFixed(digits) + " )\n";
+        else if (delta < 0) return " \t ( " + delta.toFixed(digits) + " )\n";
         else return "\n";
     }
 
@@ -65,6 +65,12 @@ class ModeStatsObject {
         else return "\n";
     }
 
+    addCompareRank(nowValue, oldValue) {
+        let delta = nowValue - oldValue;
+        if (delta > 0) return " \t ( ↓" + delta + " )\n";
+        else if (delta < 0) return " \t ( ↑" + delta * -1 + " )\n";
+        else return "\n";
+    }
 
     /**
      * @param {ModeStatsObject} oldModeStats
@@ -73,8 +79,8 @@ class ModeStatsObject {
         const dAccuracy = this.addCompareString(this.accuracy, oldModeStats.accuracy, 4);
         const dPlaycount = this.addCompareString(this.playcount, oldModeStats.playcount);
         const dLevel = this.addCompareString(this.level, oldModeStats.level, 2);
-        const dCountryRank = this.addCompareString(this.countryRank, oldModeStats.countryRank);
-        const dRank = this.addCompareString(this.rank, oldModeStats.rank);
+        //const dCountryRank = this.addCompareString(this.countryRank, oldModeStats.countryRank);
+        const dRank = this.addCompareRank(this.rank, oldModeStats.rank);
         const dPP = this.addCompareString(this.pp, oldModeStats.pp);
         const dRankedScores = this.addCompareRankedScores(this.rankedScores, oldModeStats.rankedScores);
         const dPlay_time = this.addCompareUserTimePlayed(this.play_time, oldModeStats.play_time);
@@ -83,8 +89,8 @@ class ModeStatsObject {
         output = output + "acc：" + this.accuracy + "%" + dAccuracy;
         output = output + "等级：" + this.level + dLevel;
         output = output + "pp：" + this.pp + dPP;
-        output = output + "全球排名：" + this.rank + dRank;
-        output = output + "本地排名：" + this.countryRank + dCountryRank;
+        output = output + "全服排名：#" + this.rank + dRank;
+        //output = output + "本地排名：" + this.countryRank + dCountryRank;
         output = output + "游玩次数：" + this.playcount + dPlaycount;
         output = output + "rank总分：" + utils.format_number(this.rankedScores) + dRankedScores;
         output = output + "游戏时长：" + utils.getUserTimePlayed(this.play_time) + dPlay_time;
